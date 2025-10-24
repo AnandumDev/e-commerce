756a7465000cc163199e88f6eaf8eba16158209c
Anandu <appumankunnummal@gmail.com>
2025-10-24 20:45:01 +0530

    'changed'

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'e-commerce-zeta-topaz-55.vercel.app',
     credentials: true
 }))
 app.use(express.json())
